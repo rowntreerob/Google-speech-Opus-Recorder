a0cdb2742b8c96ae9403bc16a0fb476f63b852c4
rob rowntree <rowntreerob@gmail.com>
2016-12-06 14:49:38 -0800

    init

---
+++ app/src/main/java/com/google/cloud/android/speech/OpusRecorder.java
@@ -0,0 +1,281 @@
+package com.google.cloud.android.speech;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.support.annotation.NonNull;
+
+import java.nio.ByteBuffer;
+
+import top.oply.opuslib.OpusTool;
+import top.oply.opuslib.OpusTrackInfo;
+
+
+/**
+ * Replacement for orig. 'VoiceRecorder' that adds Lib integration for recorded audio output
+ * as Opus file.
+ * Continuously records audio and notifies the {@link VoiceRecorder.Callback} when voice (or any
+ * sound) is heard.
+ *
+ * <p>The recorded audio format is always {@link AudioFormat#ENCODING_PCM_16BIT} and
+ * {@link AudioFormat#CHANNEL_IN_MONO}. This class will automatically pick the right sample rate
+ * for the device. Use {@link #getSampleRate()} to get the selected value.</p>
+ */
+public class OpusRecorder {
+
+    private static final int[] SAMPLE_RATE_CANDIDATES = new int[]{16000, 11025, 22050, 44100};
+
+    private static final int CHANNEL = AudioFormat.CHANNEL_IN_MONO;
+    private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private static final int AMPLITUDE_THRESHOLD = 1500;
+    private static final int SPEECH_TIMEOUT_MILLIS = 2000;
+    private static final int MAX_SPEECH_LENGTH_MILLIS = 30 * 1000;
+
+    public static abstract class Callback {
+
+        /**
+         * Called when the recorder starts hearing voice.
+         */
+        public void onVoiceStart() {
+        }
+
+        /**
+         * Called when the recorder is hearing voice.
+         *
+         * @param data The audio data in {@link AudioFormat#ENCODING_PCM_16BIT}.
+         * @param size The size of the actual data in {@code data}.
+         */
+        public void onVoice(byte[] data, int size) {
+        }
+
+        /**
+         * Called when the recorder stops hearing voice.
+         */
+        public void onVoiceEnd() {
+        }
+    }
+
+    private final Callback mCallback;
+
+    private AudioRecord mAudioRecord;
+
+    private Thread mThread;
+
+    private byte[] mBuffer;
+
+    private final Object mLock = new Object();
+
+    /** The timestamp of the last time that voice is heard. */
+    private long mLastVoiceHeardMillis = Long.MAX_VALUE;
+
+    /** The timestamp when the current voice is started. */
+    private long mVoiceStartedMillis;
+    private ByteBuffer firstBuffer = ByteBuffer.allocateDirect(1920);
+    private ByteBuffer fileBuffer = ByteBuffer.allocateDirect(1920);// Should be 1920, to accord with function writeFreme()
+    public OpusTool mOpusTool;
+
+    public OpusRecorder(@NonNull Callback callback) {
+        mCallback = callback;
+    }
+
+    /**
+     * Init the lib's OpusTool with a new file. writes the header in the encoded(opus) file
+     * and awaits the microphone buffer for further IO
+     * Encode PCM16 mono @16k frame/sec TO Opus 2400 mono
+     * config details of encoder? belo
+     * https://github.com/louisyonge/opus_android/blob/master/opuslib/src/main/jni/opustool/opusaudio.c#L289
+     */
+    public void init() {
+        mOpusTool = new OpusTool();
+        int rst = mOpusTool.startRecording(getNextFile());
+    }
+
+    private String getNextFile(){
+        return OpusTrackInfo.getInstance().getAValidFileName("OpusRecord");
+    }
+
+    /**
+     * Starts recording audio.
+     *
+     * <p>The caller is responsible for calling {@link #stop()} later.</p>
+     */
+    public void start() {
+        // Stop recording if it is currently ongoing.
+        stop();
+        // Try to create a new recording session.
+        mAudioRecord = createAudioRecord();
+        if (mAudioRecord == null) {
+            throw new RuntimeException("Cannot instantiate VoiceRecorder");
+        }
+        init();
+
+        // Start recording.
+        mAudioRecord.startRecording();
+        // Start processing the captured audio.
+        mThread = new Thread(new ProcessVoice());
+        mThread.start();
+    }
+
+    /**
+     * Stops recording audio and the encoderTool for Opus.
+     */
+    public void stop() {
+        synchronized (mLock) {
+            dismiss();
+            if (mThread != null) {
+                mThread.interrupt();
+                mThread = null;
+            }
+            if (mAudioRecord != null) {
+                mAudioRecord.stop();
+                mAudioRecord.release();
+                mAudioRecord = null;
+            }
+            mBuffer = null;
+            if(null != mOpusTool)mOpusTool.stopRecording();
+        }
+    }
+
+    /**
+     * Dismisses the currently ongoing utterance.
+     */
+    public void dismiss() {
+        if (mLastVoiceHeardMillis != Long.MAX_VALUE) {
+            mLastVoiceHeardMillis = Long.MAX_VALUE;
+            mCallback.onVoiceEnd();
+        }
+    }
+
+    /**
+     * Retrieves the sample rate currently used to record audio.
+     *
+     * @return The sample rate of recorded audio.
+     */
+    public int getSampleRate() {
+        if (mAudioRecord != null) {
+            return mAudioRecord.getSampleRate();
+        }
+        return 0;
+    }
+
+    /**
+     * Creates a new {@link AudioRecord}.
+     *
+     * @return A newly created {@link AudioRecord}, or null if it cannot be created (missing
+     * permissions?).
+     */
+    private AudioRecord createAudioRecord() {
+        for (int sampleRate : SAMPLE_RATE_CANDIDATES) {
+            final int sizeInBytes = AudioRecord.getMinBufferSize(sampleRate, CHANNEL, ENCODING);
+            if (sizeInBytes == AudioRecord.ERROR_BAD_VALUE) {
+                continue;
+            }
+            final AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    sampleRate, CHANNEL, ENCODING, sizeInBytes);
+            if (audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
+                mBuffer = new byte[sizeInBytes];
+                return audioRecord;
+            } else {
+                audioRecord.release();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * copy from : https://github.com/louisyonge/opus_android/blob/master/opuslib/src/main/java/top/oply/opuslib/OpusRecorder.java#L97
+     * make mic's buffer avail for call to jni layer for  OpusTool writeFrame()
+     * rev : input type DirectBuffer is now just bytes[]
+     */
+    private void writeAudioDataToOpus(byte[] buffer, int size) {
+
+        //bytes[] to new Buffer
+        ByteBuffer finalBuffer = ByteBuffer.allocateDirect(size);
+        finalBuffer.put(buffer);
+        finalBuffer.rewind();
+        boolean flush = false;
+
+        //write data  to Opus file
+        while (mVoiceStartedMillis < MAX_SPEECH_LENGTH_MILLIS && finalBuffer.hasRemaining()) {
+            int oldLimit = -1;
+            if (finalBuffer.remaining() > fileBuffer.remaining()) {
+                oldLimit = finalBuffer.limit();
+                finalBuffer.limit(fileBuffer.remaining() + finalBuffer.position());
+            }
+            fileBuffer.put(finalBuffer);
+            if (fileBuffer.position() == fileBuffer.limit() || flush) {
+                int length = !flush ? fileBuffer.limit() : finalBuffer.position();
+
+                int rst = mOpusTool.writeFrame(fileBuffer, length);
+                if (rst != 0) {
+                    fileBuffer.rewind();
+                }
+            }
+            if (oldLimit != -1) {
+                finalBuffer.limit(oldLimit);
+            }
+        }
+    }
+
+    /**
+     * Continuously processes the captured audio and notifies {@link #mCallback} of corresponding
+     * events.
+     * calls OpusEnc with bytes from microphone audio (PCM16-mono to Opus)
+     */
+    private class ProcessVoice implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (mLock) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        break;
+                    }
+                    final int size = mAudioRecord.read(mBuffer, 0, mBuffer.length);
+                    writeAudioDataToOpus(mBuffer, size);
+                    final long now = System.currentTimeMillis();
+
+                    if (isHearingVoice(mBuffer, size)) {
+                        if (mLastVoiceHeardMillis == Long.MAX_VALUE) {
+                            mVoiceStartedMillis = now;
+                            mCallback.onVoiceStart();
+                        }
+                        mCallback.onVoice(mBuffer, size);
+                        mLastVoiceHeardMillis = now;
+                        if (now - mVoiceStartedMillis > MAX_SPEECH_LENGTH_MILLIS) {
+                            end();
+                        }
+                    } else if (mLastVoiceHeardMillis != Long.MAX_VALUE) {
+                        mCallback.onVoice(mBuffer, size);
+                        if (now - mLastVoiceHeardMillis > SPEECH_TIMEOUT_MILLIS) {
+                            end();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void end() {
+            mOpusTool.stopRecording();
+            mLastVoiceHeardMillis = Long.MAX_VALUE;
+            mCallback.onVoiceEnd();
+        }
+
+        private boolean isHearingVoice(byte[] buffer, int size) {
+            for (int i = 0; i < size - 1; i += 2) {
+                // The buffer has LINEAR16 in little endian.
+                int s = buffer[i + 1];
+                if (s < 0) s *= -1;
+                s <<= 8;
+                s += Math.abs(buffer[i]);
+                if (s > AMPLITUDE_THRESHOLD) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+
+}
